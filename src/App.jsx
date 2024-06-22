8dbc762887b6db7c7b956cf26cbb9a9bc38000ee
Santi <santiagogcg@gmail.com>
2024-06-22 06:49:48 +0200

    SPT19 CH1 A CORREGIR

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
- //Aquí tu código
+    const [name, newName] = useState('Sofía')
+
+    const nameChange = (input) => {
+        newName(input.target.value)
+
+
+    }
+
+
+
+    return (
+        <div>
+            <h2>Teacher Name {name} </h2>
+            <ul>
+                <li><button value="Data" onClick={nameChange}>Data</button></li>
+                <li><button value="Reyes" onClick={nameChange}>Reyes</button></li>
+                <li><button value="Yolanda" onClick={nameChange}>Yolanda</button></li>
+            </ul>
+
+        </div>
+    )
+
 }
 
 export default App
